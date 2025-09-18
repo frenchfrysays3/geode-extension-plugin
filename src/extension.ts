3f0d6572731fdff459de116c8fd5b1b7e1db52e0
frenchfrysays <lucas.e.fry@outlook.com>
2025-09-18 13:34:08 -0500

    Pushed for codespace

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { spawn } from 'child_process';
+import fs;
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const commandBuildDefault = 'geodeplugin.builddefault';
+	const commandBuildWin = 'geodeplugin.buildwin';
+	const commandBuildMac = 'geodeplugin.buildmac';
+	const commandBuildIos = 'geodeplugin.buildios';
+	const commandBuildAndroid = 'geodeplugin.buildandroid';
+	let defaultBuildFolder;
+	let winBuildFolder;
+	let macBuildFolder;
+	let iosBuildFolder;
+	let androidBuildFolder;
+	if (fs.) {}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "geode-extension-plugin" is now active!');
+	const Output = vscode.window.createOutputChannel('Geode');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('geode-extension-plugin.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Geode Plugin!');
-	});
+	const commandBuildDefaultHandler = () => {
 
-	context.subscriptions.push(disposable);
+
+	}
+
+	context.subscriptions.push(Output);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+
+}
